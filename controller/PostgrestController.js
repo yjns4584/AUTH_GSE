226b3b2e5aa2dff833d0043ab8dec5b905b00016
Jesus-Angarita <jesusito101285@gmail.com>
2024-12-30 16:47:27 -0500

    Una que otra cosita minima y arreglos.

--- controller/PostgrestController.js
+++ controller/PostgrestController.js
@@ -1,46 +1,30 @@
-const { pgPool } = require('../db'); // Asegúrate de tener configurado el pool de PostgreSQL
-const bcrypt = require('bcrypt');
+const { pgPool } = require('../db')
+const bcrypt = require('bcrypt')
 
-/**
- * Registra un nuevo usuario en la base de datos PostgreSQL.
- * @param { object } userData Nombre de usuario.
- * @returns {Promise<{ success: boolean, message: string }>} Indica si el registro fue exitoso.
- */
 exports.registerUser = async function (userData) {
   try {
-    // Encriptar la contraseña antes de guardarla
     const hashedPassword = await bcrypt.hash(userData.password, 10);
 
-    // Inserta el usuario en la base de datos
     const query = `
-      INSERT INTO users (usuario, nombre, apellido, nacimiento, tipoCC, Cedula, email, contrasena, telefono)
+      INSERT INTO users (usuario, nombre, apellido, nacimiento, tipoCC, Cedula, email, contrasena, telefono, img64)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
       RETURNING id;
     `;
-    const values = [userData.usuario, userData.nombre, userData.apellido, userData.nacimiento, userData.tipoCC, userData.cedula, userData.email, hashedPassword, userData.telefono];
+    const values = [userData.usuario, userData.nombre, userData.apellido, userData.nacimiento, userData.tipoCC, userData.cedula, userData.email, hashedPassword, userData.telefono, userData.img64]
 
-    const result = await pgPool.query(query, values);
+    const result = await pgPool.query(query, values)
 
-    if (result.rows.length > 0) {
-      return { success: true, message: 'Usuario registrado exitosamente.' };
-    } else {
-      return { success: false, message: 'No se pudo registrar el usuario.' };
-    }
+    if (!result.rows.length > 0) return { success: false, message: 'No se pudo registrar el usuario.' }
+    return { success: true, message: 'Usuario registrado exitosamente.' }
+
   } catch (error) {
-    console.error('Error al registrar el usuario:', error.message);
-    return { success: false, message: 'Error al registrar el usuario.' };
+    console.error('Error al registrar el usuario:', error.message)
+    return { success: false, message: 'Error al registrar el usuario.' }
   }
-};
+}
 
-/**
- * Inicia sesión del usuario validando las credenciales y obteniendo sus datos.
- * @param { string } username Nombre de usuario.
- * @param { string } password Contraseña del usuario.
- * @returns {Promise<{ success: boolean, user: object | null, message: string }>} Resultado del inicio de sesión.
- */
 exports.loginUser = async function (username, password) {
   try {
-    // Consulta el usuario por nombre de usuario
     const query = `
       SELECT id, username, password, email
       FROM users
@@ -55,12 +39,10 @@ exports.loginUser = async function (username, password) {
 
     const user = result.rows[0];
 
-    // Verifica la contraseña
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) return { success: false, user: null, message: 'Contraseña incorrecta.' };
     
 
-    // Elimina la contraseña del objeto antes de retornarlo
     delete user.password;
 
     return { success: true, user, message: 'Inicio de sesión exitoso.' };
--- server.js
+++ server.js
@@ -12,17 +12,20 @@ require('./connections').mongoConnect().then( () => {
     app.use(express.json())
 
     app.post('/register', (req, res) => {
-      const userData = req.body;
+      const userData = req.body
+      console.log(userData)
+      // Tratar el JSON que me trae Kevin desde Flutter y guardar esos datos en Psostgrest.
+      res.status(200).send('Registro completado.')
       
     })
 
-    app.post('/yesid', (req, res) => {
-      console.log(req.body);
-      
-      const userData = req.body;
+    app.post('/login', (req, res) => {
+      const userLogin = req.body
+      console.log(userLogin)
+      // Con el ID del usuario, hacer un query a la base de datos y traer la imagen de la persona en BASE64 y enviarselo a Yesid.
 
-      res.status(200).send('Solicitud recibida.')
-      
+      res.status(200).send('LOGIN correcto.')
+
     })
 
     app.listen(config.port, () =>
